7fd590be1540e410f2bbb0690a2b267440ea7b0a
unknown <faotqm@hotmail.com>
2020-11-03 04:28:40 -0600

    Modificaciones a peticion del cliente

--- assets/js/dropplets.js
+++ assets/js/dropplets.js
@@ -2,39 +2,44 @@ window.onload = () => {
     let links = document.querySelectorAll('.link')
     let inicio = document.querySelector('.inicio');
     inicio.addEventListener('click', () => {
-        primero.classList.remove('link--active')
-        segundo.classList.remove('link--active')
-        tercero.classList.remove('link--active')
-        cuarto.classList.remove('link--active')
-        scrollSuave('#inicio', 1000, 84)
-    })
-    //console.log(links[6]);
-    
+            primero.classList.remove('link--active')
+            segundo.classList.remove('link--active')
+            tercero.classList.remove('link--active')
+            cuarto.classList.remove('link--active')
+            scrollSuave('#inicio', 1000, 84)
+        })
+        //console.log(links[6]);
+
     let primero = links[1]
     primero.addEventListener('click', () => {
         primero.classList.add('link--active')
         segundo.classList.remove('link--active')
         tercero.classList.remove('link--active')
         cuarto.classList.remove('link--active')
-      scrollSuave('#sobre', 1000, 81)
+        gsap.to(".about", { duration: 1, rotationX: 360 });
+        gsap.from(".texto-boton", { duration: 1, x: 300, opacity: 0 });
+        scrollSuave('#sobre', 1000, 81)
     })
-  
+
     let segundo = links[2]
     segundo.addEventListener('click', () => {
         primero.classList.remove('link--active')
         segundo.classList.add('link--active')
         tercero.classList.remove('link--active')
         cuarto.classList.remove('link--active')
-      scrollSuave('#servicio', 1000, 78)
+        gsap.from(".card_img", { duration: 1, x: 360, opacity: 1 });
+        scrollSuave('#servicio', 1000, 78)
     })
-  
+
     let tercero = links[3]
     tercero.addEventListener('click', () => {
         primero.classList.remove('link--active')
         segundo.classList.remove('link--active')
         tercero.classList.add('link--active')
         cuarto.classList.remove('link--active')
-      scrollSuave('#valores', 1000, 75)
+        gsap.from(".container-bg", { duration: 2, x: 300, opacity: 0, scale: 0.5 });
+        gsap.from(".card", { duration: 2, x: 200, ease: "bounce" });
+        scrollSuave('#valores', 1000, 75)
     })
     let cuarto = links[5]
     cuarto.addEventListener('click', () => {
@@ -42,30 +47,30 @@ window.onload = () => {
         segundo.classList.remove('link--active')
         tercero.classList.remove('link--active')
         cuarto.classList.add('link--active')
-      scrollSuave('#contacto', 1000, 72)
+        scrollSuave('#contacto', 1000, 72)
     })
-  }
-  
-  
-  const scrollSuave = (objetivo, duracion, compensacion) => {
+}
+
+
+const scrollSuave = (objetivo, duracion, compensacion) => {
     let elemObj = document.querySelector(objetivo)
     let elemPos = elemObj.getBoundingClientRect().top - compensacion
     let posInicial = window.pageYOffset
     let tiempoInicial = null
-  
+
     const animacion = tiempoAhora => {
-      if (tiempoInicial === null) tiempoInicial = tiempoAhora
-      tiempoPasado = tiempoAhora - tiempoInicial
-      let auxAnimacion = easeInOutQuad(tiempoPasado, posInicial, elemPos, duracion)
-      window.scrollTo(0, auxAnimacion)
-      if (tiempoPasado < duracion) requestAnimationFrame(animacion)
+        if (tiempoInicial === null) tiempoInicial = tiempoAhora
+        tiempoPasado = tiempoAhora - tiempoInicial
+        let auxAnimacion = easeInOutQuad(tiempoPasado, posInicial, elemPos, duracion)
+        window.scrollTo(0, auxAnimacion)
+        if (tiempoPasado < duracion) requestAnimationFrame(animacion)
     }
     requestAnimationFrame(animacion)
-  }
-  
-  const easeInOutQuad = (t, b, c, d) => {
+}
+
+const easeInOutQuad = (t, b, c, d) => {
     t /= d / 2
     if (t < 1) return c / 2 * t * t + b
     t--
-    return - c / 2 * (t * (t - 2) - 1) + b
-  }
+    return -c / 2 * (t * (t - 2) - 1) + b
+}
